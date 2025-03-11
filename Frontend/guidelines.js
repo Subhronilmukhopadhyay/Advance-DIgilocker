94ab85d2525dd468fdc0740664f1ab02f26a4612
subhronil <subhronil124@outlook.com>
2025-03-11 10:43:35 +0530

    new fixes

--- Frontend/guidelines.js
+++ Frontend/guidelines.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const container = document.querySelector('.container');
 const registerBtn = document.querySelector('.register-btn');
 const loginBtn = document.querySelector('.login-btn');
@@ -9,16 +8,4 @@ registerBtn.addEventListener('click', () => {
 
 loginBtn.addEventListener('click', () => {
     container.classList.remove('active');
-=======
-const container = document.querySelector('.container');
-const registerBtn = document.querySelector('.register-btn');
-const loginBtn = document.querySelector('.login-btn');
-
-registerBtn.addEventListener('click', () => {
-    container.classList.add('active');
-})
-
-loginBtn.addEventListener('click', () => {
-    container.classList.remove('active');
->>>>>>> 0b4e99353d5b52fa709fdd246ff22ce2ef2b18ba
 })
